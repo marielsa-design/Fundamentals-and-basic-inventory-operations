126875732bbb69ebadc4efbe1d6dee6086aba67f
marielsa-design <marielsa.milagro@gmail.com>
2026-03-11 16:04:55 -0500

    Inventario-Riwi

---
+++ Inventory.py
@@ -0,0 +1,25 @@
+# Programa to register a product in an inventory
+
+
+
+# Ask for the product name
+
+name = input("Enter the product name: ")
+
+# Validate "the Price"
+
+while True:
+    try:
+        price = float(input("Enter the product price: "))
+        break
+    except ValueError:
+        print("Invalid value. Please enter a valid number for the price.")
+
+# Validate "the Quantity"
+
+while True:
+    try:
+        quantity = int(input("Enter the product quantity: "))
+        break
+    except ValueError:
+        print("Invalid value. Please enter a valid enter for the quantity.")
--- main.py
+++ main.py
@@ -1,25 +1,51 @@
-# Programa to register a product in an inventory
+inventory = 5
+product = [ "empty"] * inventory
 
-
-
-# Ask for the product name
-
-name = input("Enter the product name: ")
-
-# Validate "the Price"
+option = 1
+print("Welcome to the Inventory System")
 
 while True:
-    try:
-        price = float(input("Enter the product price: "))
-        break
-    except ValueError:
-        print("Invalid value. Please enter a valid number for the price.")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View products")
+    print("4. Exit")
 
-# Validate "the Quantity"
-
-while True:
     try:
-        cantidad = int(input("Enter the product quantity: "))
-        break
+        option = int(input("Choose an option: "))
     except ValueError:
-        print("Invalid value. Please enter a valid enter for the quantity.")
+        print("Please enter a valid number.")
+        continue
+
+    match option:
+        case 1:
+            if product.count("empty") > 0:
+                new_product = input("Enter the product name\n").upper()
+                if new_product in product:
+                    print("That product alreday exists")
+                else:
+                    index = product.index("empty")
+                    product[index] = new_product
+                    print(f"Product '{new_product}' added successfully")
+            else:
+                print("Inventory is full")
+
+        case 2:
+            remove = input("Enter the product name to remove\n").upper()
+            if remove in product:
+                index = product.index(remove)
+                product[index] = "empty"
+                print(f"Product '{remove}' removed successfully")
+            else:
+                print("Product not found")
+
+        case 3:
+            print("\ncurrent inventory:")
+            for i, item in enumerate(product):
+                print(f" slot {i+1}: {item}")
+
+        case 4:
+            print("Goodbye!")
+            break
+
+        case _:
+            print("Invalid option. Please choose between 1 and 4")
